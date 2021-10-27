2c2bd0c68e5736d8084936b4a3a30f2319ce2295
lusqua <lucasaguiar.floripa@gmail.com>
2021-10-26 21:39:55 -0300

    Changed port

--- App.py
+++ App.py
@@ -55,4 +55,4 @@ class Todo(Resource):
 api.add_resource(Todo, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8888)
+    app.run(debug=True, host='0.0.0.0', port=443)
